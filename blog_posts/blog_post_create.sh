5da8f1ccb4f2247ba1fba90dac5103707ea14642
Daniel Alzugaray <dalzuga@gmail.com>
2016-12-25 18:04:06 -0800

    Blog post release. "To Serif or Not to Serif"   - Adding headers to bash scripts.   - Updating script to start blog post.   - Fixing footnotes.   - Styling update.   - Adding "To Serif or Not to Serif" blog post.   - Editing blog post [save]   - Editing blog post [save]   - Editing blog post [save]   - Displaying blog post. [save]   - One less line break.   - Fix paragraph.   - Remove a word.   - Remove a fragment.   - Flow of text.     - wrote some more on the content     - needed to tie in that Serif adds a touch of style   - Minor edit.

--- blog_posts/blog_post_create.sh
+++ blog_posts/blog_post_create.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+# Script for starting a blog post
+
 datetimenow="$(date "+%Y-%m-%d_%T_%z_%Z")"
+
 if [ $# -eq 1 ]
-then echo -e $datetimenow"\n# $@ [working title]" > $datetimenow"_$@"".txt"
+then
+    # make the file
+    file="$datetimenow""_$@"".md"
+    echo -e $datetimenow"\n# $@ [working title]" > "$file"
+    # echo $datetimenow"_$@"".md"
+    
+    # convert filename spaces to underscores
+    mv "$file" $(echo $file | tr ' ' '_')
+		 
 else
     echo "please provide exactly one argument (post title)"
 fi
