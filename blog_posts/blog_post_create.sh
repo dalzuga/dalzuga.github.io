58d76e3f6cc1e33b812dc1798cb42cf03a9867a7
Daniel Alzugaray <dalzuga@gmail.com>
2016-12-30 11:59:04 -0800

    Fixing build script.

--- blog_posts/blog_post_create.sh
+++ blog_posts/blog_post_create.sh
@@ -7,11 +7,16 @@ if [ $# -eq 1 ]
 then
     # make the file
     file="$datetimenow""_$@"".org"
-    echo -e $datetimenow"\n* $@ [working title]" > "$file"
+    echo -e "#+STARTUP: showall" >> "$file"
+    echo -e $datetimenow"\n* $@ [working title]" >> "$file"
     # echo $datetimenow"_$@"".md"
     
     # convert filename spaces to underscores
+    underscored_filename=$(echo $file | tr ' ' '_')
+
     mv "$file" $(echo $file | tr ' ' '_')
+    echo "Created:"
+    echo "$underscored_filename"
 		 
 else
     echo "please provide exactly one argument (post title)"
